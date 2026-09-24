feeed022da4aec7c0892d5fedb0eea8a9045fd34
Alessandro <alessandro@mediafix.it>
2026-09-24 21:25:39 +0200

    Add custom OkHttp Dispatcher to handle concurrent refresh requests

--- app/src/main/java/com/example/myapplication/data/HomeAssistantApi.kt
+++ app/src/main/java/com/example/myapplication/data/HomeAssistantApi.kt
@@ -5,6 +5,7 @@ import android.graphics.BitmapFactory
 import android.util.Log
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import okhttp3.Dispatcher
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -25,6 +26,10 @@ class HomeAssistantApi(private val prefs: HaPreferences) {
         private val client = OkHttpClient.Builder()
             .connectTimeout(15, TimeUnit.SECONDS)
             .readTimeout(15, TimeUnit.SECONDS)
+            .dispatcher(Dispatcher().apply {
+                maxRequests = 32
+                maxRequestsPerHost = 16
+            })
             .build()
     }
 
